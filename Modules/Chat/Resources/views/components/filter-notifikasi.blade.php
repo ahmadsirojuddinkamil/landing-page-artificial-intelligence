def56ae1ba43811e64cc152b85054fc3328bc3d0
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-12-28 20:22:06 +0700

    source code landing page artificial intelligence

--- Modules/Chat/Resources/views/components/filter-notifikasi.blade.php
+++
@@ -1,8 +0,0 @@
-<form method="GET" action="{{ route('ticket.cekNotif') }}" class="mb-3 form-group d-inline-flex align-items-center">
-    <label for="filter-notifikasi" class="form-label mr-2">Filter :</label>
-    <select name="notifikasi" id="filter-notifikasi" class="form-control w-auto" onchange="this.form.submit()">
-        <option value="">All</option>
-        <option value="1" {{ request('notifikasi') == 1 ? 'selected' : '' }}>Pending</option>
-        <option value="0" {{ request('notifikasi') == 0 ? 'selected' : '' }}>Respon</option>
-    </select>
-</form>
--- routes/channels.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Broadcast;
-
-// Broadcast::channel('chats.{id}', function ($user, $id) {
-//     return Auth::check();
-// });
